465532e9e999b9432679f9667165641d682d1283
Karan Vohra <karanvohra33@gmail.com>
2019-08-11 19:14:28 -0400

    replace request with node fetch and added async await

--- index.js
+++ index.js
@@ -1,51 +1,58 @@
-const request = require('request');
-const cheerio = require('cheerio');
-
+const fetch = require("node-fetch");
+const cheerio = require("cheerio");
 
 const locations = [
   {
-    url: 'http://www.espn.com/nfl/statistics/player/_/stat/receiving',
-    tableLocStr:'table tbody tr'
+    url: "http://www.espn.com/nfl/statistics/player/_/stat/receiving",
+    tableLocStr: "table tbody tr"
   },
   {
-    url: 'http://www.espn.com/fantasy/football/story/_/page/18RanksPreseason300PPR/2018-fantasy-football-ppr-rankings-top-300',
-    tableLocStr:'table tbody tr'
+    url:
+      "http://www.espn.com/fantasy/football/story/_/page/18RanksPreseason300PPR/2018-fantasy-football-ppr-rankings-top-300",
+    tableLocStr: "table tbody tr"
   },
   {
-    url: 'http://www.sportingnews.com/us/fantasy/sport/news/fantasy-football-ppr-rankings-2018-cheat-sheet-draft-strategy-tiers-sleepers-busts-targets-best-players-season-outlook-projections-top-picks/k36acqdekdl71uswdywe9t492',
-    tableLocStr:'table tbody tr'
+    url:
+      "https://www.sportingnews.com/us/fantasy/news/fantasy-ppr-rankings-top-200-cheat-sheet-point-per-reception-sleepers-busts-projections-draft-strategy-tiers-draft-kit/11feetzrpunlr1j20ww0ck5052",
+    tableLocStr: "table tbody tr"
   },
   {
-    url: 'https://www.fantasypros.com/nfl/rankings/half-point-ppr-cheatsheets.php',
-    tableLocStr: 'table#rank-data.player-table tbody tr'
+    url: "https://www.fantasypros.com/nfl/rankings/consensus-cheatsheets.php",
+    tableLocStr: "table#rank-data.player-table tbody tr"
   },
   {
-    url: 'https://draftwizard.fantasypros.com/football/cheat-sheets/rankings/?&scoring=HALF&tab=1',
-    tableLocStr: 'div.SingleCheatSheet div.LiveCheatSheet'
+    url:
+      "https://draftwizard.fantasypros.com/football/cheat-sheets/rankings/?&scoring=HALF&tab=1",
+    tableLocStr: "div.SingleCheatSheet div.LiveCheatSheet"
   }
-]
+];
 
 const scrape = async ({ url, tableLocStr }) => {
-  
-  await request(url, (error, response, html) => {
-    console.log(`------------- ${url} -----------------`)
+  try {
+    const response = await fetch(url);
+    const html = await response.text();
+    // console.log("html:", html);
+
+    console.log(`------------- ${url} -----------------`);
     const $ = cheerio.load(html);
     const table = [];
 
-    $(tableLocStr).each(function (i, el) {
-      const row = []
-      $(this).children().each(function(i, el) {
-        const text = $(this).text(); 
-        row.push(text);
-      })
-      console.log(row)
-      console.log()
+    $(tableLocStr).each(function(i, el) {
+      const row = [];
+      $(this)
+        .children()
+        .each(function(i, el) {
+          const text = $(this).text();
+          row.push(text);
+        });
+      console.log(row);
+      console.log();
       table.push(row);
     });
     return table;
-  })
-}
+  } catch (err) {
+    console.log(`Error fetching ${url}`, err);
+  }
+};
 
 locations.forEach(location => scrape(location));
-
-
