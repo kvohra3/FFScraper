15828bbb2477583ac681326b1bdc2dd8588c7ca2
Karan Vohra <karanvohra33@gmail.com>
2019-08-14 22:39:02 -0400

    add client

--- server/server.js
+++ server/server.js
@@ -1,20 +1,21 @@
 const express = require("express");
 const cors = require("cors");
-const rosterUrl = {
-  url:
-    "https://draftwizard.fantasypros.com/football/cheat-sheets/rankings/?&scoring=HALF&tab=1",
-  tableLocStr: "div.SingleCheatSheet div.LiveCheatSheet"
-};
-const { getRoster } = require("../index");
+const { getRoster } = require("./utils/index");
 
 const app = express();
 const port = 2000;
 
 app.use(cors());
 
+app.get("/", (req, res) => res.send("API is healthy"));
 app.get("/get/roster", async (req, res) => {
+  const rosterUrl = {
+    url:
+      "https://draftwizard.fantasypros.com/football/cheat-sheets/rankings/?&scoring=HALF&tab=1",
+    tableLocStr: "div.SingleCheatSheet div.LiveCheatSheet"
+  };
   const roster = await getRoster(rosterUrl);
-  console.log(roster);
   res.json(roster);
 });
+
 app.listen(port, () => console.log(`listening on port: ${port}`));
