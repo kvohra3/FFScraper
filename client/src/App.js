c534b7dbe8786be6986ee5e2034ecf31602b1f13
Karan <karanvohra33@gmail.com>
2019-08-16 19:41:29 -0400

    init table

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,16 @@
 import React from "react";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
-import logo from "./logo.svg";
 import "./App.css";
-import { TableHead } from "@material-ui/core";
 
-const generateRow = (col = 2, text = ["Test"]) => {
-  const row = [];
-  for (let i = 0; i < col; i++) {
-    row.push(<TableCell>{text[i]}</TableCell>);
-  }
-  return <TableRow>{row}</TableRow>;
-};
+import FullRoster from "./components/FullRoster/FullRoster";
+
+// const generateRow = (col = 2, text = ["Test"]) => {
+//   const row = [];
+//   for (let i = 0; i < col; i++) {
+//     row.push(<TableCell>{text[i]}</TableCell>);
+//   }
+//   return <TableRow>{row}</TableRow>;
+// };
 
 export default class App extends React.Component {
   constructor() {
@@ -22,39 +18,19 @@ export default class App extends React.Component {
     this.state = {};
   }
 
-  generateTable = (row = 2, col = 2, text = [["Test1"], ["Test2"]]) => {
-    const table = [];
-    for (let i = 0; i < row; i++) {
-      table.push(generateRow(col, text[i]));
-    }
-    console.log("table", table);
-    return table;
-  };
+  // generateTable = (row = 2, col = 2, text = [["Test1"], ["Test2"]]) => {
+  //   const table = [];
+  //   for (let i = 0; i < row; i++) {
+  //     table.push(generateRow(col, text[i]));
+  //   }
+  //   console.log("table", table);
+  //   return table;
+  // };
 
   render() {
     return (
-      <div className="App">
-        <h1 className="title">Roster</h1>
-        <Paper className="container">
-          <Table>
-            <TableBody>
-              <TableRow>
-                <TableCell rowSpan={3} />
-                <TableCell colSpan={2}>Subtotal</TableCell>
-                <TableCell align="right">Test</TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell>Tax</TableCell>
-                <TableCell align="right">Test1</TableCell>
-                <TableCell align="right">Test2</TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell colSpan={2}>Total</TableCell>
-                <TableCell align="right">Teest3</TableCell>
-              </TableRow>
-            </TableBody>
-          </Table>
-        </Paper>
+      <div className="container">
+        <FullRoster />
       </div>
     );
   }
--- client/src/components/FullRoster/FullRoster.js
+++ client/src/components/FullRoster/FullRoster.js
@@ -1,4 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import MaterialTable from "material-table";
+import Icon from "@material-ui/core/Icon";
 
-export default class FullRoster extends React.Component {}
+export default function FullRoster() {
+  const [activeRoster, setActiveRoster] = useState({
+    data: [
+      { name: "Mehmet", surname: "Baran", birthYear: 1987, birthCity: 63 },
+      { name: "Zerya Betül", surname: "Baran", birthYear: 2017, birthCity: 34 }
+    ]
+  });
+  const [selectedRow, setSelectedRow] = useState(null);
+
+  const columns = [
+    { title: "Name", field: "name" },
+    { title: "Surname", field: "surname" },
+    { title: "Birth Year", field: "birthYear", type: "numeric" },
+    {
+      title: "Birth Place",
+      field: "birthCity",
+      lookup: { 34: "İstanbul", 63: "Şanlıurfa" }
+    }
+  ];
+
+  return (
+    <MaterialTable
+      title="Editable Example"
+      columns={columns}
+      data={activeRoster.data}
+      actions={[
+        {
+          icon: () => <Icon>add</Icon>,
+          tooltip: "Add Player to team",
+          onClick: event => alert("You want to add a new row")
+        },
+        {
+          icon: () => <Icon>delete</Icon>,
+          tooltip: "Delete User",
+          onClick: event => alert("You want to delete a new row")
+        }
+      ]}
+    />
+  );
+}
