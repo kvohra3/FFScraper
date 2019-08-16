c3fabe0d88524dfabc343bc19ae87e1abd3f028d
Karan <karanvohra33@gmail.com>
2019-08-16 00:10:49 -0400

    'init'

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import logo from "./logo.svg";
+import "./App.css";
+import { TableHead } from "@material-ui/core";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const generateRow = (col = 2, text = ["Test"]) => {
+  const row = [];
+  for (let i = 0; i < col; i++) {
+    row.push(<TableCell>{text[i]}</TableCell>);
+  }
+  return <TableRow>{row}</TableRow>;
+};
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  generateTable = (row = 2, col = 2, text = [["Test1"], ["Test2"]]) => {
+    const table = [];
+    for (let i = 0; i < row; i++) {
+      table.push(generateRow(col, text[i]));
+    }
+    console.log("table", table);
+    return table;
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1 className="title">Roster</h1>
+        <Paper className="container">
+          <Table>
+            <TableBody>
+              <TableRow>
+                <TableCell rowSpan={3} />
+                <TableCell colSpan={2}>Subtotal</TableCell>
+                <TableCell align="right">Test</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Tax</TableCell>
+                <TableCell align="right">Test1</TableCell>
+                <TableCell align="right">Test2</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell colSpan={2}>Total</TableCell>
+                <TableCell align="right">Teest3</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </Paper>
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ client/src/components/FullRoster/FullRoster.js
@@ -0,0 +1,4 @@
+import React from "react";
+import MaterialTable from "material-table";
+
+export default class FullRoster extends React.Component {}
